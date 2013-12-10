7075890596ff83c5c349cf2c9b3187ff8e762206
Matteo Collina <matteo.collina@gmail.com>
2013-12-10 16:17:22 +0100

    Used minified js in production.

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
     uglify: {
       dist: {
         src: '<%= concat.dist.dest %>',
-        dest: 'build/js/<%= pkg.name %>.min.js'
+        dest: '<%= concat.dist.dest %>'
       }
     },
     less: {
